0775f530e3d3ae8caa776be43503d6917bf2c55c
PVSantos0 <paulovitorsanto@gmail.com>
2018-11-03 22:28:14 -0300

    testando outros frameworks

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+$(document).ready(function(){
+	$('.datepicker').datepicker();
+});
+
+$(document).ready(function(){
+    $('.timepicker').timepicker();
+});
+
+$('.dropdown-trigger').dropdown();
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
